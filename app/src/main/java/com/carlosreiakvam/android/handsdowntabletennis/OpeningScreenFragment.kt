3618a5a3b856d89f894c4fecec1a355c80369a4f
torsk-technologies <contact@torsktechnologies.xyz>
2022-06-06 19:47:30 +0200

    init commit

---
+++ app/src/main/java/com/carlosreiakvam/android/handsdowntabletennis/OpeningScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.carlosreiakvam.android.handsdowntabletennis
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OpeningScreenFragment : Fragment() {
+
+
+    private lateinit var viewModel: OpeningScreenViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.opening_screen_fragment, container, false)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OpeningScreenViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/carlosreiakvam/android/handsdowntabletennis/OpeningScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.carlosreiakvam.android.handsdowntabletennis
+
+import androidx.lifecycle.ViewModel
+
+class OpeningScreenViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
