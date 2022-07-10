5be7df9c695d0e442c10913b069c220fa5080345
carlosreiakvam <cre032@uit.no>
2022-07-10 16:44:07 +0200

    Add legacy splash screen

--- app/src/main/java/com/carlosreiakvam/android/handsdowntabletennis/MainActivity.kt
+++ app/src/main/java/com/carlosreiakvam/android/handsdowntabletennis/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_HandsDownTableTennis)
         setContentView(R.layout.activity_main)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
